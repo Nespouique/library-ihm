438b055311a59ce1cd87f579e121a2fd2b9e2760
Elliot HALLAIS <hallais.elliot@gmail.com>
2025-06-15 03:03:17 +0200

    [fix] Fixing header vanishing when pop in opens

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -55,7 +55,7 @@ const Layout = () => {
             </nav>
 
             {/* Main Content */}
-            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-24">
                 <Outlet />
             </main>
         </div>
