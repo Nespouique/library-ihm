1586fe15e3503f5385c3d73336a78da0b26546cf
Elliot <hallais.elliot@gmail.com>
2025-06-27 04:18:25 +0200

    [refactor] Simplifying code for fullscreen toggle

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -4,21 +4,20 @@ import { motion } from 'framer-motion';
 
 const FloatingButton = ({ onClick }) => {
     return (
-        <motion.button
-            onClick={onClick}
-            className="floating-button"
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-                delay: 0.2,
-                type: 'spring',
-                stiffness: 300,
-                damping: 20,
-            }}
-            whileTap={{ scale: 0.95 }}
-        >
-            <Plus className="h-7 w-7" />
-        </motion.button>
+        <div className="fixed bottom-8 right-8 z-50">
+            <motion.button
+                onClick={onClick}
+                className="floating-button fixed bottom-8 right-8 z-50"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    delay: 0.2,
+                    type: 'spring',
+                }}
+            >
+                <Plus className="h-7 w-7" />
+            </motion.button>
+        </div>
     );
 };
 
